8dbffbba4792208c11a7eccb8666ef91c822964c
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-12-06 13:36:17 -0300

    Limpando html

--- loja/src/app/app.module.ts
+++ loja/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClienteListaComponent } from './cliente-lista/cliente-lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteListaComponent
   ],
   imports: [
     BrowserModule,
